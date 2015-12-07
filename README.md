5392c7d53f6c135974cdc61d43b6b8bd4769c36b
Kate Zurlo-Cuva <zurlocuva@gmail.com>
2015-12-07 09:22:22 -0800

    created Pike Place data for 1st time

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cookie-stand
 Salmon cookie business for Code Fellows 201 course 12/2015
+
+This is a Web site designed to create re-produceable business projections for Pat, the salmon cookie maker. It has been designed with exceptionally simple design frameworks.
+
+The elements of the Web site were developed by Kate Zurlo-Cuva, as part of a week-long exercise for her Code Fellows 201 Course.
+
+All aspects of this site are marginally useful and can be used by anyone who desires them without concern for crediting the author.
--- cookie.js
+++ cookie.js
@@ -1,51 +1,47 @@
-var hours = [10am, 11am, noon, 1pm, 2pm, 3pm, 4pm, 5pm];
-var pikePlace = {
-  minCustomersPerHr = 17;
-  maxCustomerPerHr = 88;
-  avgCookiesPerCustomer = 5.2;
+
+  var hours = ['10am', '11am', 'noon', '1pm', '2pm', '3pm', '4pm', '5pm'];
+  var pikePlace = {
+    minCustomersPerHr: 88,
+    maxCustomerPerHr: 17,
+    avgCookiesPerCustomer: 5.2,
+    cookiesPerHourArr: [],
+    dailyTotal: 0,
+    name: "Pike Place",
+
+
 
   randCustomerPerHr: function(min, max) {
-    var randomNum = Math.floor(Math.random()(88 - 17) + 17);
-    var el = document.getElementbyID('Customers per Hour')
-  }
+    return Math.floor(Math.random() * (max - min) + min);
+  },
 
 
-    var ulEl = document.createElement('ul');
-    ulEl.appendChild(document.createTextNode('Customers per Hour'));
-//     // Generate a random number based on min and max arguments passed in when this function is called
-},
-//
   generateHourly: function() {
-    avgCookiesPerCustomer * randomNum = cookiesNeededPerHour[i];
+
     for (var i = 0; i < hours.length; i++) {
+      var cookiesNeededPerHour = Math.floor(this.avgCookiesPerCustomer * this.randCustomerPerHr(this.minCustomersPerHr,this.maxCustomerPerHr));
+      this.cookiesPerHourArr.push(cookiesNeededPerHour);
+      this.dailyTotal += cookiesNeededPerHour;
+    }},
+
+  render: function() {
+    this.generateHourly();
+    var ulEl = document.createElement('ul');
+    ulEl.appendChild(document.createTextNode(this.name));
+    var section = document.getElementById("data");
+    console.log(section);
+    section.appendChild(ulEl);
+      for (var i = 0; i < hours.length; i++) {
     var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ": " + this.cookiesPerHourArr[i];
 
-    liEl.textContent = hours[i];
-  }
+      ulEl.appendChild(liEl);
+}
 
-    ulEl.appendChild(liEl);
+}}
 
 
 
-//       // Generate a random amount of cookies (avg * random num)
-//       // Push the value to the hourly cookies array
-//       // Add the value to the daily cookie total
-//   },
-//
-//   render: function() {
-//     // Create a UL Element
-//     // Append a textNode to the UL element that will display the name of the store
-//     // Assign the store_data element to a variable; document.getElementById
-//     // Append the Ul element to your HTML document
-//
-//     // For each index in the hours array:
-//       // Create an LI element
-//       // Assign the content of the hours array and hourly cookies to the LI; '10am: 125'
-//       // Append the LI element to the UL element as a Child
-//
-//     // Create an LI element
-//     // Assign the content of your stores Daily Total to the LI element
-//     // Append the LI element to the UL as a Child
-//   }
-// };
-// // Call your render function
+
+
+
+pikePlace.render();
